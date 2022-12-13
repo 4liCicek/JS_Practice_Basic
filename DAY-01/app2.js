726c400afa01a020e49811190384fb968bfc99c1
Ali ÇİÇEK <alicicek4151@gmail.com>
2022-12-13 09:01:48 +0300

    js_p

---
+++ DAY-01/app2.js
@@ -0,0 +1,10 @@
+const renk1 = document.querySelector(".red");
+const renk2 = document.querySelector(".white");
+const renk3 = document.querySelector(".blue");
+const renk4 = document.querySelector(".black");
+
+
+renk1.innerText = "KIRMIZI";
+renk2.innerText = "BEYAZ";
+renk3.innerText = "MAVİ";
+renk4.innerText = "SİYAH";
